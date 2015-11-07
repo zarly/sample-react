921fdd3cf28ce6b48de671153b43a2890d78b689
Alexander Abramov <aabramov@richrelevance.com>
2015-11-06 16:57:33 -0800

    Initial

---
+++ components/app.jsx
@@ -0,0 +1,15 @@
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="App">
+                Hello, world! I am a App.
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('APPLICATION')
+);
